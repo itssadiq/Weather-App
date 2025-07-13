e93baf1d8dc146ae44677a24c88fa8e3d2d1a710
itssadiq <sadiqalishaikh009@gmail.com>
2025-07-13 11:34:07 +0500

    Trying to resolve http bug on secondary test branch

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 export async function fetchWeatherData(cityName) {
-  let API = `http://api.weatherapi.com/v1/forecast.json?key=b7063c358ff24027a5e155012251107&q=${cityName}&days=8&aqi=no&alerts=no
+  let API = `https://api.weatherapi.com/v1/forecast.json?key=b7063c358ff24027a5e155012251107&q=${cityName}&days=8&aqi=no&alerts=no
   `;
 
   const res = await fetch(API);
